4297d8a0993b6e7bf8c47d7c5ff7d3f2cb3c2106
Aaryan Rajora <aryan.rajora14@outlook.com>
2025-10-09 17:43:03 +0530

    Update House_Price_Prediction_App.py

--- House-Price-Prediction-with-Machine-Learning/House_Price_Prediction_App.py
+++ House-Price-Prediction-with-Machine-Learning/House_Price_Prediction_App.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import pickle
 from PIL import Image
-import plotly.express as px
 import plotly.graph_objects as go
 import joblib
 
@@ -699,4 +698,5 @@ def main():
     )
 
 if __name__ == "__main__":
+
     main()
